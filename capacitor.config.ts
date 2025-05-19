4c301968b8d6413f22a2d36c75d237e8880ce8cd
null <null>
2025-05-19 17:17:57 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mipokemonEve',
+  webDir: 'www'
+};
+
+export default config;
